1ea41cb9511a3959c0d589a4b25bdbd7e8b6e71c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2021-10-08 08:59:12 +0300

    Even or Odd

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 "use strict";
-function multiTable(number) {
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    .map((item) => `${item} * ${number} = ${item * number}`)
-    .join("\n");
-}
+const evenOrOdd = (n) => (n % 2 === 0 ? "Even" : "Odd");
